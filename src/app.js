ec94fff7b9192307416dd1631d156178a48d2615
Lincoln Kubasu <kubasulincoln@gmail.com>
2026-04-04 05:37:58 +0300

    installed the necessary packages

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 import express from 'express';
+import passport from './auth.js';
+import { sessionMiddleware } from './session.js';
 import dotenv from 'dotenv'
 import path from 'path'
 import indexRouter from './routes/index.js';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+dotenv.config();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(process.cwd(), "views"));
+
+app.use(sessionMiddleware);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/", indexRouter);
 

a8fba03f47e95629add04d2e577c065b6173429c
Lincoln Kubasu <kubasulincoln@gmail.com>
2026-04-04 05:17:23 +0300

    session setup

---
+++ src/session.js
@@ -0,0 +1,14 @@
+import session from 'express-session';
+import { PrismaSessionStore } from '@quixo3/prisma-session-store';
+import { prisma } from './prisma.js';
+
+export const sessionMiddleware = session ({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(prisma, {
+        checkPeriod: 2 * 60 * 1000, //ms
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined,
+    }),
+})

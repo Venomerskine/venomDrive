a7106346599da18410f9040580b34b7dc0efb70b
Lincoln Kubasu <kubasulincoln@gmail.com>
2026-04-04 05:26:51 +0300

    passport setup, use, serialize and deserialize

---
+++ src/auth.js
@@ -0,0 +1,37 @@
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+import { prisma } from './prisma.js';
+import bcrypt from 'bcrypt';
+
+passport.use(new LocalStrategy(
+    async (username, password, done) => {
+        try {
+            const user = await prisma.user.findUnique({ where: { username } });
+
+            if (!user) return done(null, false);
+
+            const match = await bcrypt.compare(password, user.password);
+
+            if (!match) return done(null, false);
+
+            return done(null, user);
+        } catch (err){
+            return done(err);
+        }
+    }
+))
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await prisma.user.findUnique({ where: { id } });
+        done(null, user);
+    } catch (err) {
+        done(err);
+    }
+});
+
+export default passport;

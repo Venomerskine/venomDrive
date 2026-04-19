6099c3a6ec137b5069e0db145a1afccb062d2c3c
Lincoln Kubasu <kubasulincoln@gmail.com>
2026-04-19 10:34:36 +0300

    Reearanged to bring css into action

--- src/app.js
+++ src/app.js
@@ -3,25 +3,30 @@ import passport from './auth.js';
 import { sessionMiddleware } from './session.js';
 import dotenv from 'dotenv'
 import path from 'path'
+import { fileURLToPath } from "url";
 import indexRouter from './routes/index.js';
 
+dotenv.config();
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-dotenv.config();
-
-app.set("view engine", "ejs");
-app.set("views", path.join(process.cwd(), "views"));
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
+console.log("Static path:", path.join(__dirname, "public"));
 
 app.use(sessionMiddleware);
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/uploads', express.static('uploads'));
+// app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/", indexRouter);
 
